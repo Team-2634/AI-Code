5ce003d9c7e5a0c378e0dd3971fa1887cba43e8e
Toad64 <af4toad@gmail.com>
2026-03-13 13:52:50 -0400

    auto code :p

---
+++ src/main/java/frc/robot/Auto.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class Auto {
+    private void autoDrive(){
+
+    }
+
+    private void autoTargeting(){
+
+    }
+
+    private void autoShooting(){
+
+    }
+}
--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -8,23 +8,24 @@ public class Shooter {
     public static TalonFX shooterMotor1 = new TalonFX(10);
     public static TalonFX shooterMotor2 = new TalonFX(11);
 
-    public static void shooterFowardSlow(){ // default launcher speed
+    public static void shooterForwardSlow(){ // slow launcher speed
         shooterMotor1.set(0.75);
         shooterMotor2.set(0.75);
     }
+
+    public static void shooterFowardFast(){ // fast launcher speed
+        shooterMotor1.set(1);
+        shooterMotor2.set(1);
+    }
     
-    public static void shooterFoward(double speed){ // custom launcher speed
+    public static void shooterFowardcustom(double speed){ // custom launcher speed
         shooterMotor1.set(speed);
         shooterMotor2.set(speed);
     }
 
-    public static void shooterStop(){
+    public static void shooterStop(){ // no launcher speed
         shooterMotor1.set(0);
         shooterMotor2.set(0);
     }
-
-    public static void shooterFowardFast(){ // default launcher speed
-        shooterMotor1.set(1);
-        shooterMotor2.set(1);
-    }
 }
+
