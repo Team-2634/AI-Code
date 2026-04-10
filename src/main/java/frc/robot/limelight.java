d9b47222d93017b7fcecccfc5bb92ef78bb293ec
Toad64 <af4toad@gmail.com>
2026-04-10 10:11:03 -0400

    Update limelight.java

--- src/main/java/frc/robot/limelight.java
+++ src/main/java/frc/robot/limelight.java
@@ -1,27 +1,30 @@
 package frc.robot;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-
 public class limelight {
+
+    private static Pigeon2 mainGyro = new Pigeon2(0);
 
     static double CAMERA_MOUNT_HEIGHT = 18.5; // The height of the camera from the ground (inches)
     static double CAMERA_MOUNT_Y_OFFSET = 4.5; // Y offset of camera from center of robot (inches)
     static double CAMERA_MOUNT_ANGLE = 0.0; // The angle at which the camera is mounted (degrees)
 
-    public static double getX() { // Get the horizontal offset from the crosshair to the target
+    public static double getCamX() { // Get the horizontal offset from the crosshair to the target
         return LimelightHelpers.getTX("limelight");
     }
 
-    public static double getY() { // Get the vertical offset from the crosshair to the target   
+    public static double getCamY() { // Get the vertical offset from the crosshair to the target   
         return LimelightHelpers.getTY("limelight");
     }
 
-    public static double getXNC() { // Get the horizontal offset from the camera optic to the target (normalized)
+    public static double getCamXNC() { // Get the horizontal offset from the camera optic to the target (normalized)
         return LimelightHelpers.getTXNC("limelight");
     }
 
-    public static double getYNC() { // Get the vertical offset from the camera optic to the target (normalized)
+    public static double getCamYNC() { // Get the vertical offset from the camera optic to the target (normalized)
         return LimelightHelpers.getTYNC("limelight");
     }
 
@@ -29,18 +32,23 @@ public class limelight {
         return LimelightHelpers.getTA("limelight");
     }
 
-    public static double robotX(){
-        return robotY() * (Math.tan(getXNC() * (Math.PI/180)));
+    public static double getRobotX(){
+        return getRobotY() * (Math.tan(getCamXNC() * (Math.PI/180)));
     }
 
-    public static double robotY(){
-        return (limelightTagData.getTagHeight(getTagId()) - CAMERA_MOUNT_HEIGHT) / (Math.tan(getYNC() * (Math.PI/180))) + CAMERA_MOUNT_Y_OFFSET;
+    public static double getRobotY(){
+        return (limelightTagData.getTagHeight(getTagId()) - CAMERA_MOUNT_HEIGHT) / (Math.tan(getCamYNC() * (Math.PI/180))) + CAMERA_MOUNT_Y_OFFSET;
     }
 
-    public static double[] RobotXY(){
-        double y = (limelightTagData.getTagHeight(getTagId()) - CAMERA_MOUNT_HEIGHT) / (Math.tan(getYNC() * (Math.PI/180))) + CAMERA_MOUNT_Y_OFFSET;
-        double x = y * (Math.tan(getXNC() * (Math.PI/180)));
-        return new double[]{x, y};
+    public static double[] getRobotPos(){
+        double camY = getRobotY();
+        double camX = camY * (Math.tan(getCamXNC() * (Math.PI/180)));
+        double camYaw = mainGyro.getYaw().getValueAsDouble();
+
+        double robotPosX = camX * Math.cos(camYaw) - camY * camX * Math.sin(camYaw);
+        double robotPosY = camX * Math.sin(camYaw) - camY * camX * Math.cos(camYaw);
+
+        return new double[]{robotPosX, robotPosY};
     }
 
     private static final NetworkTable limelight = 
@@ -50,15 +58,26 @@ public class limelight {
         return (int) limelight.getEntry("tid").getDouble(-1);
     }
 
-    public static void dumpTargetData(){ // Print all the data to the console
+    public static void dumpAllTargetData(){ // Print all data outputs to the console
         System.out.println(
             " ID: " + getTagId() + 
-            " X: " + getX() + 
-            " Y: " + getY() + 
-            " XNC: " + getXNC() + 
-            " YNC: " + getYNC() + 
+            " X: " + getCamX() + 
+            " Y: " + getCamY() + 
+            " XNC: " + getCamXNC() + 
+            " YNC: " + getCamYNC() + 
             " Area: " + getArea() +
-            " Position " + RobotXY()
+            " Robot X " + getRobotX() +
+            " Robot Y " + getRobotY() +
+            " Position " + getRobotPos()
+            );
+    }
+
+    public static void printImportantData() { // Prints only the important data.
+        System.out.println(
+            " ID: " + getTagId() + 
+            " XNC: " + getCamXNC() + 
+            " YNC: " + getCamYNC() + 
+            " Position " + getRobotPos()
             );
     }
 }
